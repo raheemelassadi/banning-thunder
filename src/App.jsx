8253900b1896c71c4f459c39fb95f9f7c35dbb57
raheemelassadi <raheemelassadi1@gmail.com>
2023-08-25 23:51:27 -0700

    finished hero section, made it mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,43 @@ function App() {
           <h1 className="cursor-pointer">Menu</h1>
         </nav>
         <div className="flex justify-center flex-col items-center box-border">
-          <h1 className="px-28  text-2xl md:text-6xl text-white font-Jakarta font-semibold text-center drop-shadow-lg mb-11 mt-16">
-            Thrift Today, <br /> Thrive Tomorrow
+          <h1 className="px-28 text-3xl md:text-5xl text-white font-Jakarta font-semibold text-center drop-shadow-lg mb-11 mt-28 md:w-[650px] w-[490px]">
+            Thrift Today, Thrive Tomorrow
           </h1>
-          <p className=" text-sm md:text-lg text-white w-2/5 text-center font-medium mb-20">
+          <p className=" text-xs w-full md:px-0 px-14 md:text-lg text-white md:w-2/5 text-center font-medium mb-16">
+            Embrace thriftiness now for a brighter future. Explore incredible
+            deals at our thrift shop for amazing treasures at unbeatable prices.
+            Selling to us sustains thriftiness and supports your future.
+          </p>
+          <button className="w-40 px-3 py-2 text-sm md:text-md rounded-full bg-yellow-500 md:w-44 mb-20 md:py-3 md:px-9 hover:bg-opacity-80 box-border">
+            Contact Us
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center ">
+        <p>About Us</p>
+        <div className="flex justify-center gap-11 md:flex-row flex-col">
+          <h3 className="md:w-1/3 px-28 md:px-6">
             Embrace thriftiness today to secure a better tomorrow. Discover
             unbeatable deals at our thrift shop, where you can find treasures at
             amazing prices. Plus, when you sell your items to us, you contribute
             to a sustainable cycle of thrift and support your future endeavors.
             Thrift Today, Thrive Tomorrow.
-          </p>
-          <button className="rounded-full bg-yellow-500 w-44 mb-20 py-3 px-9 hover:bg-opacity-80 box-border">
-            Contact Us
-          </button>
+          </h3>
+          <h3 className="md:w-1/3 px-28 md:px-6">
+            Embrace thriftiness today to secure a better tomorrow. Discover
+            unbeatable deals at our thrift shop, where you can find treasures at
+            amazing prices. Plus, when you sell your items to us, you contribute
+            to a sustainable cycle of thrift and support your future endeavors.
+            Thrift Today, Thrive Tomorrow.
+          </h3>
+          <h3 className="md:w-1/3 px-28 md:px-6">
+            Embrace thriftiness today to secure a better tomorrow. Discover
+            unbeatable deals at our thrift shop, where you can find treasures at
+            amazing prices. Plus, when you sell your items to us, you contribute
+            to a sustainable cycle of thrift and support your future endeavors.
+            Thrift Today, Thrive Tomorrow.
+          </h3>
         </div>
       </div>
     </>
